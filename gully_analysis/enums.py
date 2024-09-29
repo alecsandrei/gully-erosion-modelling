87544cc4695a8910c6a9f85b726601567dce7176
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2024-09-29 08:58:28 +0300

    Algorithm enum with a class method to turn it into a display name

--- gully_analysis/enums.py
+++ gully_analysis/enums.py
@@ -2,5 +2,15 @@ from enum import Enum
 
 
 class PluginName(Enum):
-    LONG = 'Gully Analysis'
-    SHORT = 'gullyanalysis'
+    DISPLAY_NAME = 'Gully Analysis'
+    SHORT_NAME = 'gullyanalysis'
+
+
+class Algorithm(Enum):
+    ESTIMATE_EROSION_FUTURE = 'estimatefuture'
+
+    @classmethod
+    def display_name(cls) -> str:
+        return ' '.join(
+            [part.capitalize() for part in cls._name_.split('_')]
+        )
