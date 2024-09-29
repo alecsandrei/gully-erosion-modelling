a88b9a1e132321fbc5b2b8e3b820bc81cb6d8db3
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2024-09-29 20:39:29 +0300

    main.py moved to estimate erosion future processing algorithm

--- gully_analysis/processing/algorithms/estimate_erosion_future.py
+++ gully_analysis/processing/algorithms/estimate_erosion_future.py
@@ -1,29 +1,43 @@
 from __future__ import annotations
 
-from qgis.PyQt.QtCore import QCoreApplication
+from pathlib import Path
+import typing as t
+
+from qgis.PyQt.QtCore import QCoreApplication  # type: ignore
 from qgis.core import (
     QgsProcessing,
-    QgsFeatureSink,
+    Qgis,
     QgsProcessingAlgorithm,
+    QgsProcessingException,  # type: ignore
+    QgsProcessingParameterBoolean,
     QgsProcessingParameterFeatureSource,
     QgsProcessingParameterRasterLayer,
-    QgsProcessingParameterFeatureSink
+    QgsProcessingFeedback,
+    QgsProcessingContext,
+    QgsProcessingParameterFileDestination,
 )
 
-from ...enums import (
-    Algorithm,
-    AlgorithmGroup
+from ...enums import Algorithm, AlgorithmGroup
+from ...utils import get_first_geometry, geometries_to_layer
+from ...geometry import (
+    Centerlines,
+    Endpoints,
+    intersection_points,
 )
+from ...graph import build_graph
 
 
 class EstimateErosionFuture(QgsProcessingAlgorithm):
 
-    GULLY_BOUNDARY = 'GULLY_BOUNDARY'
-    GULLY_ELEVATION = 'GULLY_ELEVATION'
-    GULLY_FUTURE_BOUNDARY = 'GULLY_FUTURE_BOUNDARY'
+    GULLY_BOUNDARY = "GULLY_BOUNDARY"
+    GULLY_ELEVATION = "GULLY_ELEVATION"
+    GULLY_FUTURE_BOUNDARY = "GULLY_FUTURE_BOUNDARY"
+    CENTERLINES = "CENTERLINES"
+    DEBUG_MODE = "DEBUG_MODE"
+    OUTPUT = "OUTPUT"
 
     def tr(self, string):
-        return QCoreApplication.translate('Processing', string)
+        return QCoreApplication.translate("Processing", string)
 
     def createInstance(self):
         return EstimateErosionFuture()
@@ -42,8 +56,8 @@ class EstimateErosionFuture(QgsProcessingAlgorithm):
 
     def shortHelpString(self):
         return self.tr(
-            'Used to estimate the eroded volume near the gully head '
-            'for a future date.'
+            "Used to estimate the eroded volume near the gully head "
+            "for a future date."
         )
 
     def initAlgorithm(self, config=None):  # type: ignore
@@ -52,32 +66,142 @@ class EstimateErosionFuture(QgsProcessingAlgorithm):
         self.addParameter(
             QgsProcessingParameterFeatureSource(
                 self.GULLY_BOUNDARY,
-                self.tr('The gully boundary'),
-                [QgsProcessing.TypeVectorPolygon]
+                self.tr("The gully boundary"),
+                [QgsProcessing.TypeVectorPolygon],
             )
         )
 
         self.addParameter(
             QgsProcessingParameterRasterLayer(
                 self.GULLY_ELEVATION,
-                self.tr('The gully elevation raster'),
+                self.tr("The gully elevation raster"),
             )
         )
 
         self.addParameter(
             QgsProcessingParameterFeatureSource(
                 self.GULLY_FUTURE_BOUNDARY,
-                self.tr('The gully future boundary'),
-                [QgsProcessing.TypeVectorPolygon]
+                self.tr("The gully future boundary"),
+                [QgsProcessing.TypeVectorPolygon],
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFeatureSource(
+                self.CENTERLINES,
+                self.tr("The gully future boundary centerlines"),
+                [QgsProcessing.TypeVectorLine],
+                optional=True,
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterBoolean(
+                self.DEBUG_MODE,
+                self.tr("Debug mode (more logs and intermediary files)"),
+                [QgsProcessing.TypeVectorLine],
             )
         )
 
         # TODO: add output
 
-    def processAlgorithm(self, parameters, context, feedback):
+    def processAlgorithm(
+        self,
+        parameters: dict[str, t.Any],
+        context: QgsProcessingContext,
+        feedback: QgsProcessingFeedback | None = None,
+    ):
+        # TODO: prevent the code from breaking if feedback is None
+        assert feedback is not None
+        project = context.project()
+        if project is None:
+            QgsProcessingException("Failed to retrieve the project.")
+        assert project is not None
+        crs = project.crs()
+        if crs is None:
+            QgsProcessingException("Failed to fetch the CRS from the project.")
+        gully_boundary = self.parameterAsVectorLayer(
+            parameters, self.GULLY_BOUNDARY, context
+        )
+        gully_elevation = self.parameterAsRasterLayer(
+            parameters, self.GULLY_ELEVATION, context
+        )
+        gully_future_boundary = self.parameterAsVectorLayer(
+            parameters, self.GULLY_FUTURE_BOUNDARY, context
+        )
+        centerlines = self.parameterAsVectorLayer(
+            parameters, self.CENTERLINES, context
+        )
+        debug_mode = self.parameterAsBool(parameters, self.DEBUG_MODE, context)
+        gully_polygon = get_first_geometry(gully_boundary).coerceToType(
+            Qgis.WkbType.Polygon
+        )[0]
+        gully_limit = gully_polygon.coerceToType(Qgis.WkbType.MultiLineString)[
+            0
+        ]
+        gully_future_polygon = get_first_geometry(
+            gully_future_boundary
+        ).coerceToType(Qgis.WkbType.Polygon)[0]
 
-        gully_boundary = parameters[self.GULLY_BOUNDARY]
-        gully_elevation = parameters[self.GULLY_ELEVATION]
-        gully_future_boundary = parameters[self.GULLY_FUTURE_BOUNDARY]
+        difference = gully_future_polygon.difference(gully_polygon)
+        limit_difference = difference.coerceToType(
+            Qgis.WkbType.MultiLineString
+        )[0]
 
-        print(gully_boundary, gully_elevation, gully_future_boundary)
+        if centerlines is None:
+            # Leaving a 'TEMPORARY_OUTPUT' here will create a geopackage
+            # instead, which breaks the output file for me (QGIS 3.38.3)
+            # By creating a temporay shapefile path via
+            # QgsProcessingParameterFileDestination, we make sure that the
+            # output will be valid
+            temp_path = Path(
+                QgsProcessingParameterFileDestination(
+                    name="centerlines"
+                ).generateTemporaryDestination(context)
+            ).with_suffix(".shp")
+            centerlines = Centerlines.compute(
+                gully_future_boundary, context, feedback, temp_path
+            )
+            if debug_mode:
+                feedback.pushDebugInfo(f"Saved centerlines at {temp_path}")
+                project.addMapLayer(centerlines._layer)  # type: ignore
+                feedback.pushDebugInfo("Added centerlines in the project")
+
+        else:
+            centerlines = Centerlines.from_layer(centerlines)
+
+        pour_points = []
+        for centerline in centerlines.intersects(difference):
+            first, last = Endpoints.from_linestring(
+                centerline
+            ).as_qgis_geometry()
+            if (
+                first.intersects(limit_difference)
+                and not last.intersects(gully_limit)
+                and not first.intersects(gully_limit)
+            ):
+                pour_points.append(first)
+
+        points_intersecting_gully_boundary = intersection_points(
+            centerlines, gully_polygon
+        )
+
+        if debug_mode:
+            feedback.pushDebugInfo("Building graph to merge the centerlines.")
+        shortest_paths = list(
+            build_graph(
+                pour_points,
+                centerlines._layer,
+                points_intersecting_gully_boundary,
+            )
+        )
+        if debug_mode:
+            feedback.pushDebugInfo("Graph built.")
+        shortest_paths_as_layer = geometries_to_layer(
+            shortest_paths, "shortest_paths"
+        )
+        shortest_paths_as_layer.setCrs(crs)
+        if debug_mode:
+            project.addMapLayer(shortest_paths_as_layer)
+
+        return {self.OUTPUT: None}
