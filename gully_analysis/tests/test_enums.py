f3769906abdc9beb946494604e2e22aff738eddd
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2024-09-29 09:56:18 +0300

    tests for enums

---
+++ gully_analysis/tests/test_enums.py
@@ -0,0 +1,28 @@
+import unittest
+
+from gully_analysis.enums import (
+    Displayable,
+    Assets
+)
+
+
+class TestEnums(unittest.TestCase):
+
+    def test_display_name(self):
+
+        class TestEnum(Displayable):
+            TEST_ONE = 'testone'
+            TESTTWO = 'testtwo'
+
+        self.assertEqual(
+            TestEnum.TEST_ONE.display_name(),
+            'Test One'
+        )
+        self.assertEqual(
+            TestEnum.TESTTWO.display_name(),
+            'Testtwo'
+        )
+
+    def test_assets(self):
+        for path in Assets:
+            self.assertTrue(path.value.exists())
