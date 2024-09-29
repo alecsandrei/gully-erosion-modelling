84ba8ff99c59d1caecbdf2885c98f9cae7493f50
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2024-09-29 07:58:42 +0300

    added a PluginName enum to be used by the processing provider class

---
+++ gully_analysis/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class PluginName(Enum):
+    LONG = 'Gully Analysis'
+    SHORT = 'gullyanalysis'
--- gully_analysis/processing_provider/provider.py
+++ gully_analysis/processing_provider/provider.py
@@ -1,23 +1,18 @@
+from gully_analysis.enums import PluginName
+
 from qgis.core import QgsProcessingProvider
 
 
 class GullyErosionEstimator(QgsProcessingProvider):
 
     def loadAlgorithms(self, *args, **kwargs):
-        self.addAlgorithm(ExampleProcessingAlgorithm())
-        # add additional algorithms here
-        # self.addAlgorithm(MyOtherAlgorithm())
+        ...
 
     def id(self, *args, **kwargs):
-        return 'gullyerosionestimator'
+        return PluginName.SHORT.value
 
     def name(self, *args, **kwargs):
-        """The human friendly name of your plugin in Processing.
-
-        This string should be as short as possible (e.g. "Lastools", not
-        "Lastools version 1.0.1 64-bit") and localised.
-        """
-        return self.tr('Your plugin')
+        return self.tr(PluginName.LONG.value)
 
     def icon(self):
         """Should return a QIcon which is used for your provider inside
