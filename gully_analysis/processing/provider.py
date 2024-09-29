52f36bb86251c9b6e4483c2cedfe4a9ca81bc94b
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2024-09-29 09:58:59 +0300

    switched to new enum behaviour

--- gully_analysis/processing/provider.py
+++ gully_analysis/processing/provider.py
@@ -1,18 +1,19 @@
-from ..enums import PluginName
-
 from qgis.core import QgsProcessingProvider
+
+from ..enums import PluginName
+from .algorithms import EstimateErosionFuture
 
 
 class GullyAnalysisProvider(QgsProcessingProvider):
 
     def loadAlgorithms(self, *args, **kwargs):
-        ...
+        self.addAlgorithm(EstimateErosionFuture())
 
     def id(self, *args, **kwargs):
-        return PluginName.SHORT.value
+        return PluginName.GULLY_ANALYSIS.value
 
     def name(self, *args, **kwargs):
-        return self.tr(PluginName.LONG.value)
+        return self.tr(PluginName.GULLY_ANALYSIS.display_name())
 
     def icon(self):
         return QgsProcessingProvider.icon(self)
